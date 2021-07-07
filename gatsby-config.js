2b76f3f76a26f2a24d1142a0f03d62e406b9f4f7
Muhammad Faizullah Syarizzat <muhdfaizul@mail.ru>
2021-07-08 06:38:12 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
         manualInit: true,
-        modulePath: `src/cms/cms.js`, // This needs to be relative to the .docz directory
+        modulePath: `https://github.com/faizulho/gatsby-starter-docz-netlifycms-1/src/cms/cms.js', // This needs to be relative to the .docz directory
       },
     }
   ]
